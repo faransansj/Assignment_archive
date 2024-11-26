8c239cefac06e6c1760cd25ed343fe98d072ffdc
Midori <46928147+faransansj@users.noreply.github.com>
2024-11-26 12:51:10 +0900

    Create test1.cpp

---
+++ 프로그래밍방법론/week12/test1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string str1("Hello, World!");
+	cout << str1 << endl;
+	cout << "length : " << str1.length() << endl;
+	cout << "size : " << str1.size() << endl;
+	return 0;
+}
